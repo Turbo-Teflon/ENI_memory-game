042b59c332eaaaa0c148bff02c201d0e16f32238
Turbo-Teflon <marwan.neuman@gmail.com>
2025-05-09 09:24:58 +0200

    add connexion token

---
+++ scripts/connect.js
@@ -0,0 +1,39 @@
+const email = document.getElementById("email");
+const password = document.getElementById("mdp");
+const submitBtn = document.getElementById("submitBtn");
+const emailError = document.getElementById("emailError");
+const mdpError = document.getElementById("mdpError");
+
+const MDP_ERROR = "mdp invalide";
+const MAIL_ERROR = "email invalide, inscrivez-vous";
+
+submitBtn.addEventListener("click", () => {
+  const users = JSON.parse(localStorage.getItem("users"));
+  let userExist = false;
+
+  if (users) {
+
+    users.forEach((user) => {
+
+      if (user.mail === email.value) {
+        userExist = true;
+
+        if (password.value === user.mdp) {
+          sessionStorage.setItem("currentUser", JSON.stringify(user));
+					alert("Connexion Réussie, bienvenu " + user.nom);
+        } else {
+          mdpError.textContent = MDP_ERROR;
+          mdpError.classList.remove("success");
+          mdpError.classList.add("error");
+        }
+      }
+
+    });
+    if (!userExist){
+      emailError.textContent = MAIL_ERROR;
+      emailError.classList.add("error");
+      mdpError.classList.remove("success");
+    }
+
+  }
+});
